3b678f4c70b5cbb80bb968576045a84492b52534
Patrick <pdougall1@gmail.com>
2014-11-06 17:07:41 -0500

    refactors linkedin client
    * now uses mostly methods on the instance

--- app/models/linkedin/client.rb
+++ app/models/linkedin/client.rb
@@ -9,8 +9,17 @@ class Linkedin::Client
 	end
 
 	def self.exchange_token(code)
-		response = instance.connection.post do |req|
-		  req.url instance.access_token_exchange_url
+		instance.exchange_token(code)
+	end
+
+	def self.find(query_type, access_token)
+		instance.find(query_type, access_token)
+	end
+
+	def exchange_token(code)
+		@domain = 'https://www.linkedin.com'
+		response = connection.post do |req|
+		  req.url access_token_exchange_url
 		  req.params['grant_type'] = 'authorization_code'
 		  req.params['code'] = code
 		  req.params['redirect_uri'] = 'https://localhost:5000/sessions/linkedin'
@@ -20,21 +29,36 @@ class Linkedin::Client
 		response.body
 	end
 
-	def connection
-		@connection ||= Faraday.new(:url => domain) do |faraday|
-		  faraday.request  :url_encoded             # form-encode POST params
-		  faraday.response :logger                  # log requests to STDOUT
-		  faraday.response :json, content_type: /\bjson$/
-		  faraday.adapter Faraday.default_adapter
-		end 	
+	def find(query_type, access_token)
+		@domain = 'https://api.linkedin.com'
+		response = connection.get do |req|
+		  req.url send(query_type.to_sym)
+		  req.headers['x-li-format'] = "json"
+		  req.headers['Authorization'] = "Bearer #{access_token}"
+		end
+		response.body
 	end
 
-	def domain
-		'https://www.linkedin.com'
+	def connection
+		@connection = faraday.new(:url => @domain) do |config|
+		  config.request  :url_encoded             # form-encode POST params
+		  config.response :logger                  # log requests to STDOUT
+		  config.response :xml,  content_type: /\bxml$/
+		  config.response :json, content_type: /\bjson$/
+		  config.adapter faraday.default_adapter
+		end 	
 	end
 
 	def access_token_exchange_url
 		'/uas/oauth2/accessToken'
+	end
+
+	def person_url
+		'/v1/people'
+	end
+
+	def me
+		"#{person_url}/~"
 	end
 
 end
