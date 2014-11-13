c972f09d9d536b31fe2d2da3c7558c5e04d912d7
Patrick <pdougall1@gmail.com>
2014-11-13 10:46:35 -0500

    redirects to hashed url from sessions#create
    This is sadly necessary when serving static pages…
    
    right now.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ActionController::API
 	def create
 		session = SessionCreator.create(params[:code])
 		user = UserCreator.create(session)
-		redirect_to "#{ENV['DOUGALL_EMBER_DOMAIN']}/friends-graph/#{user.id}"
+		redirect_to "#{ENV['DOUGALL_EMBER_DOMAIN']}/#/friends-graph/#{user.id}"
 	end
 
 end
