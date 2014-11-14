55941b9d73e987a64b4c3c70dd844887496eee3c
Patrick <pdougall1@gmail.com>
2014-11-13 23:08:30 -0500

    simplifies users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ActionController::API
 
 	def show
-		if user = User.find(params[:id])
-			render json: user.serealize
-		else
-			head :ok
-		end
+		user = User.find(params[:id])
+		render json: user.serealize
 	end
 
 end
